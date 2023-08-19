4067c45bfa04a5de057c62f894a1100683b712d8
spython <emmanuelegbo01@gmail.com>
2023-08-19 15:54:18 +0100

    created a 'route' folder for users

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('Connected to MongoDB'))
+.catch((err) => {console.error(err);});
+
+
+//MIDDLEWARE
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to Homepage")
+});
+app.get("/users",(req,res)=>{
+    res.send("Welcome to user page")
+});
 
 
 app.listen(8800, () => {
-    console.log("Backend server is runing...")
+    console.log("Backend server is running...")
 });
