d1fa6e6ab9361cb78563cabe5d34bd9e166e4de3
spython <emmanuelegbo01@gmail.com>
2023-08-20 07:52:45 +0100

    created an auth folder with auth routes

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -18,13 +19,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to Homepage")
-});
-app.get("/users",(req,res)=>{
-    res.send("Welcome to user page")
-});
-
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is running...")
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/",(req,res)=>{
-    res.send("hey, it's user route")
+    res.send("hey, it's auth route")
 })
 
 module.exports = router;
